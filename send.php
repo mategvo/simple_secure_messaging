e986ff3836de1f8940dd3af1b812bc9c37ec16b4
mate <m@digit.st>
2017-03-02 17:07:43 +0100

    simple fixes

--- send.php
+++ send.php
@@ -15,8 +15,8 @@ $msg = $_POST["message"];
 $keyRegex = '/-----BEGIN PGP PUBLIC KEY BLOCK-----([\sa-zA-Z+\/\+0-9\=\s]*)([\=\sa-zA-Z0-9]*)\s-----END PGP PUBLIC KEY BLOCK-----/';
 $msgRegex = '/-----BEGIN PGP MESSAGE-----([\sa-zA-Z+\/\+0-9\=\s]*)([\=\sa-zA-Z0-9]*)\s-----END PGP MESSAGE-----/';
 
-$keyValid = preg_match_all($keyRegex, $key);
-$msgValid = preg_match_all($msgRegex, $msg);
+$keyValid = preg_match($keyRegex, $key);
+$msgValid = preg_match($msgRegex, $msg);
 
 if($keyValid < 1){
 	echo '<h3 class="r">Invalid public key</h3>';
@@ -27,11 +27,12 @@ if($msgValid < 1){
 }
 
 if($keyValid > 0 && $msgValid > 0){
-	$query = ("INSERT INTO `ssm`.`ssm` (`id`,`pgp_key`, `message`) VALUES ('$id','$key', '$msg')");
+	$query = ("INSERT INTO `ssm` (`id`,`pgp_key`, `message`) VALUES ('$id', '$key', '$msg')");
 	if($link->query($query)){
 		echo '<h3 class="g">Message sent</h3>';
 	}else{
 		echo 'Error sending message';
+		echo '<p class="r">'.$link->error.'</p>';
 	}
 }
 
